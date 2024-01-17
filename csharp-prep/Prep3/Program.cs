6512c8d0d37e61563bddad2f361e6972744206ed
TylerHavel <hav17005@byui.edu>
2024-01-17 12:44:42 -0700

    added code to complete assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Gathering Information for parts 1 & 2 where magic number is chosen
+        Console.WriteLine("What is the magic number? ");
+        int magicNum = int.Parse(Console.ReadLine());
+        int guess = -1;
+        // Loop to guide the user higher or lower if guess is wrong
+        while (guess != magicNum)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! ");
+            }
+
+        }
     }
+
 }
